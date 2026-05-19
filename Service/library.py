8ec333daa76ae51331dd3bbe938d5b5c07102aec
Sultanali Baltabai <sultanalibaltabaj5@gmail.com>
2026-05-19 09:37:10 +0500

    добавил выдачу книг

--- Service/library.py
+++ Service/library.py
@@ -8,3 +8,27 @@ class Library:
 
     def find_user(self, user_id):
         return self.users.get(user_id)
+
+    def borrow_book(self, user_id, book_id):
+        user = self.find_user(user_id)
+        if user is None:
+            print("User not found.")
+            return False
+
+        book = self.find_book(book_id)
+        if book is None:
+            print("Book not found.")
+            return False
+
+        if not book.is_available():
+            print(f"Book '{book.get_title()}' is not available.")
+            return False
+
+        if book_id in user.get_borrowed_books():
+            print(f"User already has this book.")
+            return False
+
+        book.set_available(False)
+        user.borrow_book(book_id)
+        print(f"'{book.get_title()}' successfully borrowed by {user.get_name()}.")
+        return True
